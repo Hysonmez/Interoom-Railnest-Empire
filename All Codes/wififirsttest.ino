703fd5e4e6d3e0c902491a5c76d3832121ccce07
Hysonmez <halimyigithansonmez@gmail.com>
2026-06-25 23:15:11 +0300

    Add files via upload

---
+++ All Codes/wififirsttest.ino
@@ -0,0 +1,42 @@
+#include <WiFi.h>
+
+const char* ssid = "SSid";
+const char* password = "Password";
+
+unsigned long lastTry = 0;
+const long retryInterval = 5000; 
+
+void connectWiFi() {
+  Serial.println("WiFi connecting...");
+  WiFi.disconnect();
+  WiFi.begin(ssid, password);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  connectWiFi();
+}
+
+void loop() {
+
+  if (WiFi.status() != WL_CONNECTED) {
+
+    if (millis() - lastTry > retryInterval) {
+      lastTry = millis();
+      Serial.println("Trying again...");
+      connectWiFi();
+    }
+
+  } else {
+
+    static unsigned long lastPrint = 0;
+
+    if (millis() - lastPrint > 3000) {
+      lastPrint = millis();
+      Serial.print("Connected. IP: ");
+      Serial.println(WiFi.localIP());
+    }
+  }
+}
